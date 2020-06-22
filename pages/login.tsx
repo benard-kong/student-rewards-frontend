b16a29ee625581e387cd86039e5fcfda0eb25414
Benard Kong <benardkong@hotmail.com>
2020-06-21 22:13:00 -0700

    [styling-v1] Styling Login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,7 +2,22 @@ import React from "react";
 import { useRouter } from "next/router";
 import { useMutation } from "@apollo/react-hooks";
 import gql from "graphql-tag";
+import { Button, Container, TextField, styled } from "@material-ui/core";
 import { withApollo } from "../apollo/apollo";
+
+const OuterContainer = styled(Container)({
+  "text-align": "center",
+});
+
+const InputsContainer = styled(Container)({
+  display: "flex",
+  "flex-direction": "column",
+  "font-size": "1rem",
+});
+
+const Input = styled(TextField)({
+  "margin-bottom": "1em",
+});
 
 const LOGIN_MUTATION = gql`
   mutation login($email: String!, $password: String!) {
@@ -31,11 +46,30 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email" />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="password" />
-      <button type="submit">Login</button>
-    </form>
+    <OuterContainer>
+      <h1>Please enter your email and password to log in.</h1>
+      <form onSubmit={handleSubmit}>
+        <InputsContainer maxWidth="sm">
+          <Input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            label="Email"
+            variant="outlined"
+          />
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            label="Password"
+            variant="outlined"
+          />
+          <Button type="submit" variant="contained" color="primary">
+            Login
+          </Button>
+        </InputsContainer>
+      </form>
+    </OuterContainer>
   );
 };
 
