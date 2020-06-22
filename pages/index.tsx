c8447d150a7c16a7d1250a950c519e85df26455e
Benard Kong <benardkong@hotmail.com>
2020-06-21 21:46:06 -0700

    [styling-v1] Styling splash page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,27 @@
 import React from "react";
+import Link from "next/link";
 import styled from "styled-components";
+import { Container } from "@material-ui/core";
 import { withApollo } from "../apollo/apollo";
 
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
+const ContentContainer = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  font-size: 1rem;
+  text-align: center;
 `;
 
 const IndexPage: React.FC = () => {
   return (
-    <div>
-      <Title>Hello World</Title>
-    </div>
+    <Container>
+      <ContentContainer>
+        <h1>Welcome to the Student Rewards Website</h1>
+        <p>
+          Are you a teacher at this school? Please <Link href="/login">sign in</Link>.
+        </p>
+      </ContentContainer>
+    </Container>
   );
 };
 
