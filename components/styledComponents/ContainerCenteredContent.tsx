e2a992a7bdcb3dba2b24b9614882f56e42a02cc4
Benard Kong <benardkong@hotmail.com>
2020-06-22 17:32:26 -0700

    [styling-v1] Styling for all students page

---
+++ components/styledComponents/ContainerCenteredContent.tsx
@@ -0,0 +1,7 @@
+import { Container, styled } from "@material-ui/core";
+
+const ContainerCenteredContent = styled(Container)({
+  "text-align": "center",
+});
+
+export default ContainerCenteredContent;
--- pages/student/all.tsx
+++ pages/student/all.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { useQuery } from "@apollo/react-hooks";
+import { Container, styled } from "@material-ui/core";
 import { withApollo } from "../../apollo/apollo";
 import { QUERY_ALL_STUDENTS } from "../../graphql/studentQueries";
 import { Student } from "../../typeDefs/typeDefs";
 import { compareStudents } from "../../utils/compareFunctions";
+
+const Wrapper = styled(Container)({
+  "text-align": "center",
+});
 
 const AllStudentsPage: React.FC = () => {
   const router = useRouter();
@@ -22,17 +27,16 @@ const AllStudentsPage: React.FC = () => {
   const students = allStudents.sort(compareStudents);
 
   return (
-    <div>
+    <Wrapper>
+      <h1>Students List</h1>
       {students.map((student) => (
-        <div key={student.id}>
-          <Link href="/student/[id]" as={student.id} passHref>
-            <a>
-              <h2>{`${student.lastName}, ${student.firstName}`}</h2>
-            </a>
-          </Link>
-        </div>
+        <Link key={student.id} href="/student/[id]" as={student.id} passHref>
+          <a>
+            <h2>{`${student.lastName}, ${student.firstName}`}</h2>
+          </a>
+        </Link>
       ))}
-    </div>
+    </Wrapper>
   );
 };
 
