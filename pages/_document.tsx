8f2c5752a1b7725ff4def5f22b3ccea8d4155e67
Benard Kong <benardkong@hotmail.com>
2020-06-22 19:23:17 -0700

    [styling-v1] Add MuiServerStyleSheets to _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,29 +1,34 @@
-import Document, { DocumentContext } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+/* eslint react/jsx-props-no-spreading: 0 */
+import React from "react";
+import Document, { DocumentContext } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+import { ServerStyleSheets as MuiServerStyleSheets } from "@material-ui/core";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const muiSheets = new MuiServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-        })
+          enhanceApp: (App) => (props) => muiSheets.collect(<App {...props} />),
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
           <>
             {initialProps.styles}
             {sheet.getStyleElement()}
+            {muiSheets.getStyleElement()}
           </>
         ),
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 }
