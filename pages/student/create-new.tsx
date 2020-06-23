43ff85a4a78ff4f44f48eb6425ad673d7c95f841
Benard Kong <benardkong@hotmail.com>
2020-06-22 18:19:34 -0700

    [styling-v1] Styling for create new student page

--- pages/student/create-new.tsx
+++ pages/student/create-new.tsx
@@ -1,7 +1,32 @@
 import React from "react";
 import { useMutation } from "@apollo/react-hooks";
+import styled from "styled-components";
+import { Box, Button, TextField, styled as muiStyled, Typography } from "@material-ui/core";
+import ContainerCenteredContent from "../../components/styledComponents/ContainerCenteredContent";
 import { withApollo } from "../../apollo/apollo";
 import { CREATE_STUDENT } from "../../graphql/studentMutations";
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const InputContainer = muiStyled(Box)({
+  display: "flex",
+
+  "&:not(last-of-type)": {
+    "margin-bottom": "0.8em",
+  },
+});
+
+const Input = muiStyled(TextField)({
+  flex: "1",
+});
+
+const ErrorComponent = muiStyled(Typography)({
+  color: "#f00",
+  "margin-top": "1em",
+});
 
 const firstNameError = "First name is required";
 const lastNameError = "Last name is required";
@@ -40,21 +65,55 @@ const CreateNewStudentPage: React.FC = () => {
   if (loading) return <h1>Loading...</h1>;
 
   return (
-    <div>
-      {errorMessage && <h1>Error: {errorMessage}</h1>}
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="First name" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-        <input type="text" placeholder="Last name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-        <input
-          type="text"
-          placeholder="Student number"
-          value={studentNumber}
-          onChange={(e) => setStudentNumber(e.target.value)}
-        />
-        <input type="text" placeholder="Grade" value={grade} onChange={(e) => setGrade(e.target.value)} />
-        <button type="submit">Create New Student</button>
-      </form>
-    </div>
+    <ContainerCenteredContent>
+      <h1>Fill in the details below to create a new student account.</h1>
+      <Form onSubmit={handleSubmit}>
+        <InputContainer>
+          <Input
+            type="text"
+            label="Student's First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            variant="outlined"
+            autoFocus
+          />
+        </InputContainer>
+        <InputContainer>
+          <Input
+            type="text"
+            label="Student's Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            variant="outlined"
+            autoFocus
+          />
+        </InputContainer>
+        <InputContainer>
+          <Input
+            type="text"
+            label="Student Number"
+            value={studentNumber}
+            onChange={(e) => setStudentNumber(e.target.value)}
+            variant="outlined"
+            autoFocus
+          />
+        </InputContainer>
+        <InputContainer>
+          <Input
+            type="text"
+            label="Student's Grade or Year (1-12)"
+            value={grade}
+            onChange={(e) => setGrade(e.target.value)}
+            variant="outlined"
+            autoFocus
+          />
+        </InputContainer>
+        <Button type="submit" variant="contained" color="primary">
+          Create New Student
+        </Button>
+      </Form>
+      {errorMessage && <ErrorComponent variant="h5">Error: {errorMessage}</ErrorComponent>}
+    </ContainerCenteredContent>
   );
 };
 
